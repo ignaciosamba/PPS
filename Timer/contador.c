551a5cffbf6fecb5ff834eae732025f44d1a7439
Ignacio <ignaciosamba@gmail.com>
2015-06-10 17:01:45 -0300

    ok

--- Timer/contador.c
+++ Timer/contador.c
@@ -128,8 +128,7 @@ void main (void)
 
    // printf("holaaaaa\n");
 
-   if(TL0 > 0)
-   {
+   
       rawValue.Byte[Byte3] = 0x00;
       rawValue.Byte[Byte2] = 0x00;
       rawValue.Byte[Byte1] = (unsigned char)TH0;
@@ -138,8 +137,8 @@ void main (void)
       res = rawValue.result;
       
       printf("Contador : %lu\n", res);
-   }
    
+
    }                          // Loop forever
 }
 
@@ -236,32 +235,34 @@ int iniciar_UART(void)
                                       //        clear RI0 and TI0 bits
    if (SYSCLK/BAUDRATE/2/256 < 1) 
    {
-      TH1 = -(SYSCLK/BAUDRATE/2);
-      CKCON &= ~0x0B;                  // T1M = 1; SCA1:0 = xx
+      TMR2H = -(SYSCLK/BAUDRATE/2);
+      CKCON &= ~0x22;                  // T1M = 1; SCA1:0 = xx
       CKCON |=  0x08;
    } 
    else if (SYSCLK/BAUDRATE/2/256 < 4) 
    {
-      TH1 = -(SYSCLK/BAUDRATE/2/4);
-      CKCON &= ~0x0B;                  // T1M = 0; SCA1:0 = 01                  
+      TMR2H = -(SYSCLK/BAUDRATE/2/4);
+      CKCON &= ~0x22;                  // T1M = 0; SCA1:0 = 01                  
       CKCON |=  0x01;
    } 
    else if (SYSCLK/BAUDRATE/2/256 < 12) 
    {
-      TH1 = -(SYSCLK/BAUDRATE/2/12);
-      CKCON &= ~0x0B;                  // T1M = 0; SCA1:0 = 00
+      TMR2H = -(SYSCLK/BAUDRATE/2/12);
+      CKCON &= ~0x22;                  // T1M = 0; SCA1:0 = 00
    } 
    else 
    {
-      TH1 = -(SYSCLK/BAUDRATE/2/48);
-      CKCON &= ~0x0B;                  // T1M = 0; SCA1:0 = 10
+      TMR2H = -(SYSCLK/BAUDRATE/2/48);
+      CKCON &= ~0x22;                  // T1M = 0; SCA1:0 = 10
       CKCON |=  0x02;
    }
 
-   TL1 = TH1;                          // Init Timer1
-   TMOD &= ~0xf0;                      // TMOD: timer 1 in 8-bit autoreload
-   TMOD |=  0x20;                       
-   TR1 = 1;                            // START Timer1
+   TMR2L = TMR2H;                          // Init Timer1
+   // TMOD &= ~0xf0;                      // TMOD: timer 1 in 8-bit autoreload
+   TMR2CN = 0x0C;
+
+   // TMOD |=  0x20;                       
+   TR2 = 1;                            // START Timer1
    TI0 = 1;                            // Indicate TX0 ready
 
    return 0;
