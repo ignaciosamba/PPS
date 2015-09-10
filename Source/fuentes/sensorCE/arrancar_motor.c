f2390f5bdc7800b7a792d526c2afde4046e7ca5d
LuchoManto <lucianomanto@gmail.com>
2015-09-10 11:43:39 -0300

    este es el pwm del ejemplo de silabs

--- Source/fuentes/sensorCE/arrancar_motor.c
+++ Source/fuentes/sensorCE/arrancar_motor.c
@@ -1,284 +1,274 @@
+
+
+//-----------------------------------------------------------------------------
+// F35x_PCA0_16Bit_PWM_Output.c
+//-----------------------------------------------------------------------------
+// Copyright 2006 Silicon Laboratories, Inc.
+// http://www.silabs.com
+//
+// Program Description:
+//
+// This program sends a PWM waveform out of an I/O pin, using the PCA's
+// 16-bit PWM Output Mode.  The duty cycle of the waveform is modified
+// periodically within the main loop.
+//
+// In this example, PCA Module 0 is used to generate the waveform, and the
+// crossbar is configured to send the CEX0 pin out on P0.0.
+//
+// How To Test:
+//
+// 1) Download code to a 'F35x device which has an oscilloscope monitoring P0.0
+// 2) Run the program - the waveform should be visible on the oscilloscope.
+// 3) Verify that the duty cycle of the waveform varies smoothly between
+//    minimum and maximum values.
+//
+//
+// FID:            35X000014
+// Target:         C8051F35x
+// Tool chain:     Keil C51 8.0 / Keil EVAL C51
+// Command Line:   None
+//
+//
+// Release 1.0
+//    -Initial Revision (BD)
+//    -3 AUG 2006
+//
+
+//-----------------------------------------------------------------------------
+// Includes
+//-----------------------------------------------------------------------------
+
 #include <stdio.h>
 #include "../headers.h"
 
-#define PINCELDA 3
-#define PINMOTOR 12
-#define SETPOINT 40
-#define PWMDEARRANQUE 1340
-
-sbit PIN_PWM = P1^1;
-sbit LED = P0^6;
-
-volatile short int veces_pwm=0;
-volatile short int velocidad=PWMDEARRANQUE;
-short int cont_pwm=0;
-short int variable=1000;
-short int cantInt;
-char f_PWM;
-bool f_UART;
-
-void delay(int microsegundos)
-{
-	int i = 0;
-	while(1)
-	if(f_PWM)
-	{
-		f_PWM = false;
-		i++;
-		if(i < microsegundos/125) break;	
-	}
-}
-
-void iniciar_UART(void)
-{
 
 //-----------------------------------------------------------------------------
-// UART0_Init
+// Global Constants
+//-----------------------------------------------------------------------------
+
+#define SYSCLK       24500000          // Internal oscillator frequency in Hz
+
+//-----------------------------------------------------------------------------
+// Function Prototypes
+//-----------------------------------------------------------------------------
+
+void OSCILLATOR_Init (void);
+void PORT_Init (void);
+void PCA0_Init (void);
+
+//-----------------------------------------------------------------------------
+// Global Variables
+//-----------------------------------------------------------------------------
+
+unsigned int CEX0_Compare_Value;       // Holds current PCA compare value
+
+//-----------------------------------------------------------------------------
+// main() Routine
+//-----------------------------------------------------------------------------
+
+void main (void) 
+{
+   unsigned char delay_count;          // Used to implement a delay
+   bit duty_direction = 0;             // 0 = Decrease; 1 = Increase
+
+   PCA0MD = 0x00;                      // Disable watchdog timer
+
+   PORT_Init ();                       // Initialize crossbar and GPIO
+   OSCILLATOR_Init ();                 // Initialize oscillator
+   PCA0_Init ();                       // Initialize PCA0
+
+   EA = 1;                             // Globally enable interrupts
+
+   while (1)
+   {
+      // Wait a little while
+      for (delay_count = 120; delay_count > 0; delay_count--);
+
+      if (duty_direction == 1)         // Direction = Increase
+      {
+         // First, check the ECOM0 bit
+         if ((PCA0CPM0 & 0x40) == 0x00)
+         {
+            PCA0CPM0 |= 0x40;          // Set ECOM0 if it is '0'
+         }
+         else                          // Increase duty cycle otherwise
+         {
+            CEX0_Compare_Value--;      // Increase duty cycle
+
+            if (CEX0_Compare_Value == 0x0000)
+            {
+               duty_direction = 0;     // Change direction for next time
+            }
+         }
+      }
+      else                             // Direction = Decrease
+      {
+         if (CEX0_Compare_Value == 0xFFFF)
+         {
+            PCA0CPM0 &= ~0x40;         // Clear ECOM0
+            duty_direction = 1;        // Change direction for next time
+         }
+         else
+         {
+            CEX0_Compare_Value++;      // Decrease duty cycle
+         }
+      }
+
+   };
+}
+
+
+//-----------------------------------------------------------------------------
+// Initialization Subroutines
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+// OSCILLATOR_Init
 //-----------------------------------------------------------------------------
 //
-// Configure the UART0 using Timer1, for <BAUDRATE> and 8-N-1.
+// Return Value : None
+// Parameters   : None
 //
-   
-   XBR0     |= 0x01;                    // Enable UART to Pins P0.4, P0.5
-   XBR1     |= 0x40;                    // Enable Crossbar 
-
-   P0SKIP   = 0x00;                    // Skip No Port Pins
-   P0MDOUT |= 0x10;                    // Enable UTX as push-pull output
-   P0MDIN  |= 0x20;					   // Enable URX pin as digital input.
-   
-   
-   
-   SCON0 = 0x10;                       // SCON0: 8-bit variable bit rate
-                                       //        level of STOP bit is ignored
-                                       //        RX enabled
-                                       //        ninth bits are zeros
-                                       //        clear RI0 and TI0 bits
-   if (SYSCLK/BAUDRATE/2/256 < 1) {
-      TH1 = -(SYSCLK/BAUDRATE/2);
-      CKCON |=  0x08;                  // T1M = 1; SCA1:0 = xx
-   } else if (SYSCLK/BAUDRATE/2/256 < 4) {
-      TH1 = -(SYSCLK/BAUDRATE/2/4);
-      CKCON &= ~0x0B;                  // T1M = 0; SCA1:0 = 01                  
-      CKCON |=  0x01;
-   } else if (SYSCLK/BAUDRATE/2/256 < 12) {
-      TH1 = -(SYSCLK/BAUDRATE/2/12);
-      CKCON &= ~0x0B;                  // T1M = 0; SCA1:0 = 00
-   } else {
-      TH1 = -(SYSCLK/BAUDRATE/2/48);
-      CKCON &= ~0x0B;                  // T1M = 0; SCA1:0 = 10
-      CKCON |=  0x02;
-   }
-
-   TL1 = TH1;                          // init Timer1
-   TMOD &= ~0xf0;                      // TMOD: timer 1 in 8-bit autoreload
-   TMOD |=  0x20;                       
-   TR1 = 1;                            // START Timer1
-   TI0 = 1;                            // Indicate TX0 ready
-
-
-}
-
-void iniciar_puertos (void)
+// This function initializes the system clock to use the internal oscillator
+// at 24.5 MHz.
+//
+//-----------------------------------------------------------------------------
+void OSCILLATOR_Init (void)
 {
-	XBR0     = 0x01;                    // Habilitar UART en P0.4(TX) y P0.5(RX)                     
-	XBR1     = 0x51;                    // Habilitar el crossbar, habilitar Timer0 y PCA
-	P0MDOUT |= 0x10;                    // Habilitar UTX como push-pull output
-	P1MDOUT |= 0x02;					// Habilitar el pin del motor P1.1 como push pull output
-	// P0MDIN |= 0x0C;                  // P0.3 y P0.4 tienen que ser entrada digital
+   OSCICN = 0x83;                      // Set internal oscillator to run
+                                       // at its maximum frequency
 
-
-	P0MDOUT |= 0x40;					// Habilitar P0.6 el led como push pull
+   CLKSEL = 0x00;                      // Select the internal osc. as
+                                       // the SYSCLK source
 }
 
-void iniciar_sysclock (void)
+//-----------------------------------------------------------------------------
+// PORT_Init
+//-----------------------------------------------------------------------------
+//
+// Return Value : None
+// Parameters   : None
+//
+// This function configures the crossbar and GPIO ports.
+//
+// P0.0   digital   push-pull     PCA0 CEX0
+//
+//-----------------------------------------------------------------------------
+void PORT_Init (void)
 {
-   OSCICN |= 0x03;                     // configuracion del oscilador para la maxima frecuencia
-   RSTSRC  = 0x04;                     // Enable missing clock detector
+   XBR0    = 0x00;
+   XBR1    = 0x41;                     // Route CEX0 to P0.0,
+                                       // Enable crossbar and weak pull-ups
+
+   P0MDOUT |= 0x01;                    // Set CEX0 (P0.0) to push-pull
 }
 
-void iniciar_arrancaMotor(void) // usa timer2
+//-----------------------------------------------------------------------------
+// PCA0_Init
+//-----------------------------------------------------------------------------
+//
+// Return Value : None
+// Parameters   : None
+//
+// This function configures the PCA time base, and sets up 16-bit PWM output
+// mode for Module 0 (CEX0 pin).
+//
+// The frequency of the PWM signal generated at the CEX0 pin is equal to the
+// PCA main timebase frequency divided by 65536.
+//
+// The PCA time base in this example is configured to use SYSCLK, and SYSCLK
+// is set up to use the internal oscillator running at 24.5 MHz.  Therefore,
+// the frequency of the PWM signal will be 24.5 MHz / 65536 = 373.8 Hz.
+// Using different PCA clock sources or a different processor clock will
+// result in a different frequency for the PWM signal.
+//
+//    -------------------------------------------------------------------------
+//    How "16-Bit PWM Mode" Works:
+//
+//       The PCA's 16-bit PWM Mode works by setting an output pin low every
+//    time the main 16-bit PCA counter (PCA0H) overflows, and then setting
+//    the pin high whenever a specific match condition is met.
+//
+//    Upon a PCA0 overflow (PCA0 incrementing from 0xFFFF to 0x0000), the
+//    CEXn pin will be set low.
+//
+//    When the PCA0 register increments and matches the PCA0CPn register for
+//    the selected module, the CEXn pin will be set high, except when the
+//    ECOMn bit in PCA0CPMn is cleared to '0'.  By varying the value of the
+//    PCA0CPn register, the duty cycle of the waveform can also be varied.
+//
+//    When ECOMn = '1', the duty cycle of the PWM waveform is:
+//
+//       16-bit PWM Duty Cycle = (65536 - PCA0CPn) / 65536
+//
+//    To set the duty cycle to 100%, a value of 0x0000 should be loaded into
+//    the PCA0CPn register for the module being used (with ECOMn set to '1').
+//    When the value of PCA0CPn is equal to 0x0000, the pin will never be
+//    set low.
+//
+//    To set the duty cycle to 0%, the ECOMn bit in the PCA0CPMn register
+//    should be cleared to 0.  This prevents the PCA0CPn match from occuring,
+//    which results in the pin never being set high.
+//
+// When adjusting the PWM duty cycle, the low byte of the PCA0CPn register
+// (PCA0CPLn) should be written first, followed by the high byte (PCA0CPHn).
+// Writing the low byte clears the ECOMn bit, and writing the high byte will
+// restore it.  This ensures that a match does not occur until the full
+// 16-bit value has been written to the compare register.  Writing the high
+// byte first will result in the ECOMn bit being set to '0' after the 16-bit
+// write, and the duty cycle will also go to 0%.
+//
+// It is also advisable to wait until just after a match occurs before
+// updating the PWM duty cycle.  This will help ensure that the ECOMn
+// bit is not cleared (by writing PCA0CPLn) at the time when a match was
+// supposed to happen.  This code implements the compare register update in
+// the PCA ISR upon a match interrupt.
+//    -------------------------------------------------------------------------
+//
+//-----------------------------------------------------------------------------
+void PCA0_Init (void)
 {
+   // Configure PCA time base; overflow interrupt disabled
+   PCA0CN = 0x00;                      // Stop counter; clear all flags
+   PCA0MD = 0x08;                      // Use SYSCLK as time base
 
-	TMR2CN |= (1 << 6); //flag de overflow habilitada
-	TMR2CN |= (1 << 5); // habilito interrupcion de byte inferior
-	TMR2CN |= (1 << 3); // modo split
+   PCA0CPM0 = 0xCB;                    // Module 0 = 16-bit PWM mode and
+                                       // enable Module 0 Match and Interrupt
+                                       // Flags
 
-	//clock interno / 12
+   // Configure initial PWM duty cycle = 50%
+   CEX0_Compare_Value = 65536 - (65536 * 0.5);
 
-	// CKCON  &=~ (1 << 5);
-	CKCON  &=~ (1 << 4); // registro bajo de timer2 definido por T2XCLK (sysclock / 12)
+   PCA0CPL0 = (CEX0_Compare_Value & 0x00FF);
+   PCA0CPH0 = (CEX0_Compare_Value & 0xFF00)>>8;
 
-    IE |= (1 << 5); //habilitar interrupcion de timer2
-    IP |= (1 << 5); // dar prioridad a interrupcion de timer2
+   EIE1 |= 0x10;                       // Enable PCA interrupts
 
-	TMR2L = 0;
-
-    //se calculo un valor de retorno de 65536 - 64 = 65280
-    TMR2RLL = 0; //parte baja del valor de retorno de timer2.
+   // Start PCA counter
+   CR = 1;
 }
 
-/**
- * @brief define cada cuanto se envia un pulso por la salida
- * @details setea el valor de frecuencia de envio de pulsos segun un numero ingresado que representa los microsegundos
- */
-void set_cantInt(int microsegundos)
+//-----------------------------------------------------------------------------
+// PCA0_ISR
+//-----------------------------------------------------------------------------
+//
+// Return Value : None
+// Parameters   : None
+//
+// This is the ISR for the PCA.  It handles the case when a match occurs on
+// channel 0, and updates the PCA0CPn compare register with the value held in
+// the global variable "CEX0_Compare_Value".
+//
+//-----------------------------------------------------------------------------
+void PCA0_ISR (void) interrupt 11
 {
-	cantInt = microsegundos / 125;
- /*
- *  EXPLICACION DE ESTA FUNCION
- *  ----------------------------
- *  
- *	usando timer2 en modo 16 bits con registros de autoreload y clockeado por sysclock / 12
- *	
- *	se tiene que 
- *	
- *			 SYSCLOK / 12
- *			--------------  = cantidad de interrupciones / segundo
- *			 256 - RLV
- *	
- *	con RLV el valor de retorno de los registros de reload de timer2
- *
- *	siendo la fuente del timer2, SYSCLCK / 12 = 2041666 y el valor de reload = 0, 
- *	actualmente hay 2041666 / 256 = 7975 interrupciones / segundo. Este valor se puede usar para calcular el 
- *	periodo minimo del PWM, ya que contando interrupciones, se puede alargar el periodo. 
- *	
- *	Si no se cuenta ninguna interrupcion, es decir, si se toman todas, tenemos un periodo de 125 us, y este es el
- *	periodo minimo que se puede obtener para este valor de retorno de los registros de reload. 
- *	
- *	
- *	teniendo en cuenta esto, se tiene que
- *	
- *	siendo Y = cantidad de veces que se interrumpe hasta que se levanta la bandera
- *	siendo X = valor en uS del periodo para el generador de pulsos
- *	
- *	y tomando X como el parametro de entrada en la funcion, e Y como la salida
- *	
- *	se obtiene Y sabiendo que el periodo minimo es 125 uS:
- *
- *	   		 _______________
- *	  		|               |
- *    ==>	|  Y = X/125    | 
- *    		|_______________|
- *    
- */
+   CCF0 = 0;                           // Clear module 0 interrupt flag.
+
+   PCA0CPL0 = (CEX0_Compare_Value & 0x00FF);
+   PCA0CPH0 = (CEX0_Compare_Value & 0xFF00)>>8;
 }
 
-void modificarPwm(int microsegundos)
-{
-	set_cantInt(microsegundos); // cambio el valor de cantInt, y despues lo uso para contar las interrupciones y saber en cual me salgo.
-	veces_pwm = 20000/microsegundos;
-}
 
-void arrancar_motor(void)
-{
-	printf(".\n");
-	modificarPwm(1150);
-	delay(1500000);
-	printf("..\n");
-	modificarPwm(1200);
-	delay(1500000);
-	printf("...\n");
-	modificarPwm(velocidad);
-	delay(1500000);
-}
-
-//rutina de interrupcion de timer2
- void T2_ISR(void) interrupt 5
- {
-	// printf( "LOW: 0x%x\n", (char)TMR2L);
- 	// TMR2CN &= ~(1 << 7); // volver a 0 la bandera de interrupcion del byte superior
- 	TMR2CN &= ~(1 << 6); // volver a 0 la bandera de interrupcion del byte inferior
- 	f_PWM = true; // se hablilita la bandera del timer
-	// printf( "HIGH: 0x%x\n", (char)TMR2H);
- }
-
-void UART_ISR (void) interrupt 4
-{
-	if(RI0 == 1)
-	{                         // Se borra el bit de dato recibido en la UART
-		RI0 = 0;
-		f_UART = true;
-	}
-//LED = ~LED;
-}
-
-void main()
-{
-   	int i = 0;
-   	int j = 0;
-   	char done = 0;
-   	PCA0MD &= ~0x40;                    // deshabilitar el watchdog timer
-   	iniciar_sysclock();
-   	iniciar_puertos();
-   	iniciar_UART();
-   	// printf("HOLA\n");
-   	iniciar_arrancaMotor();
-   	// printf("lala\n");
-
-   	EA = 1; //habilitar interrupciones globales
-
-   	arrancar_motor();
-   	printf("arranca\n");
-
-   	while(1)
-   	{
-
-   		ES0 = 1; // habilitar interrupcion de UART
-		if (f_UART)
-	    {
-	    	EA = 0; // inhabilitar interrupciones globales
-	    	f_UART = 0;
-	    	velocidad = velocidad + 20;
-   			modificarPwm(velocidad);
-   			printf("%d\n", velocidad);
-	    	EA = 1; // habilitar interrupciones globales
-	    }
-	    ES0 = 0;
-
-
-
-   		if(f_PWM)
-   		{
-   			i++;
-   			if(!done)
-   				j++;
-
- 			f_PWM = 0; // se deshablilita la bandera del timer}
- 			if(i >= cantInt)
- 			{
- 				i = 0;
-
- 				  if(cont_pwm==(veces_pwm))
-				  {
-				      PIN_PWM = 1;
-				      cont_pwm=0; 
-				  }
-				  else
-				  {
-				      PIN_PWM = 0;
-				  }
-				  cont_pwm++;
- 			}
-
- 			if(j == 16000)
- 			{
- 				printf("SUBE\n");
-   				modificarPwm(1700);
- 			}
- 			if(j == 24000)
- 			{
- 				printf("estable\n");
-   				modificarPwm(1499);
-   				done = 1;
-   				j = 0;
- 			}
-
-
-   		}   	
-   	}
-
-
-
-
-}
-
+//-----------------------------------------------------------------------------
+// End Of File
+//-----------------------------------------------------------------------------
